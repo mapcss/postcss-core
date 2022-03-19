3c4b738f12b2698c08e6e19232713eee2b15c870
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-03-19 14:00:23 +0900

    chore(_tools): fix publish script

--- _tools/publish_npm.ts
+++ _tools/publish_npm.ts
@@ -1,5 +1,5 @@
 import { prerelease, valid } from "https://deno.land/x/semver@v1.4.0/mod.ts";
-import { cleanVersion, constructMeta } from "./meta.ts";
+import { cleanVersion, makeOptions } from "./meta.ts";
 
 if (import.meta.main) {
   const version = Deno.args[0];
@@ -15,14 +15,12 @@ if (import.meta.main) {
   const isPrerelease = prerelease(cleanVersion(version));
   const tag = isPrerelease?.[0] ?? "latest";
 
-  const packages = constructMeta(version);
-  await Promise.all(packages.map(async (pkg) => {
-    const result = await Deno.run({
-      cmd: ["npm", "publish", pkg.outDir, "--tag", String(tag)],
-      stdout: "piped",
-    })
-      .output();
+  const pkg = makeOptions(version);
+  const result = await Deno.run({
+    cmd: ["npm", "publish", pkg.outDir, "--tag", String(tag)],
+    stdout: "piped",
+  })
+    .output();
 
-    console.log(new TextDecoder().decode(result));
-  }));
+  console.log(new TextDecoder().decode(result));
 }
